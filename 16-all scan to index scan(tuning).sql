4f43f5c000812f824e318f32c71e4892bf15a269
starryeye <see2818@naver.com>
2024-08-31 18:24:51 +0900

    [starryeye, 2024.08.31] - all scan to index scan
    내용
    - all scan to index scan

---
+++ 16-all scan to index scan(tuning).sql
@@ -0,0 +1,56 @@
+DROP table if exists users;
+
+create table users (
+	id int auto_increment primary key,
+	name varchar(100),
+	department varchar(100),
+	salary int,
+	created_at timestamp default current_timestamp
+);
+
+
+-- 깊은 재귀를 허용하도록 설정
+SET SESSION cte_max_recursion_depth = 1000000;
+
+-- 더미데이터 100만개 생성
+-- 100만개의 row 를 가지는 cte 라는 임시 테이블을 생성하고 해당 컬럼 값들은 랜덤 값으로 구성
+-- cte 의 모든 데이터를 users 에 집어 넣는다.
+INSERT INTO users (name, department, salary, created_at)
+WITH RECURSIVE cte (n) AS
+(
+	SELECT 1
+	UNION ALL
+	SELECT n + 1 FROM cte WHERE n < 1000000
+)
+SELECT 
+	CONCAT('User', LPAD(n, 7, '0')), -- 'User' 다음에 7자리 숫자로 구성된 이름 생성 
+	CASE
+		WHEN n % 10 = 1 THEN 'Engineering'
+		WHEN n % 10 = 2 THEN 'Marketing'
+		WHEN n % 10 = 3 THEN 'Sales'
+		WHEN n % 10 = 4 THEN 'Finance'
+		WHEN n % 10 = 5 THEN 'Human Resourcs'
+		WHEN n % 10 = 6 THEN 'Operations'
+		WHEN n % 10 = 7 THEN 'Information Technology'
+		WHEN n % 10 = 8 THEN 'Customer Service'
+		WHEN n % 10 = 9 THEN 'Research and Development'
+		ELSE 'Product Management'
+	END AS department, -- 부서이름 생성 
+	FLOOR(1 + RAND() * 1000000) AS salary, -- 1부터 1000000 사이의 랜덤 값으로 급여 생성
+	TIMESTAMP(DATE_SUB(NOW(), INTERVAL FLOOR(RAND() * 3650) DAY) + INTERVAL FLOOR(RAND() * 86400) SECOND) AS created_at -- 최근 10년 내의 날짜 생성 
+FROM cte;
+
+
+-- Question, all type scan.. (모든데이터 접근후 sort 과정도 해주는중)
+explain analyze SELECT * from users
+	order by salary
+	LIMIT 100;
+	
+
+-- 인덱스 생성
+CREATE index idx_salary on users(salary);
+
+-- index type scan
+explain analyze SELECT * from users
+	order by salary -- 인덱스를 활용했기 때문에 sort 작업을 할 필요가 없어졌고 조회해야할 데이터도 100개로 확 줄었다. (index type scan) 
+	LIMIT 100; -- limit 이 없으면 옵티마이저가 all type scan 을 해버린다. (optimizer may not always use an index 참고)
